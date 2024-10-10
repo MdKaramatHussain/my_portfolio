73c3ff338b69aaf9aeb20946b9ea6fa1a8259eb7
MdKaramatHussain <karamathussain0288@gmail.com>
2024-10-11 01:20:18 +0530

    my first commit for portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+import { motion } from 'framer-motion'
+import { Tilt } from 'react-tilt'
+import { styles } from '../style'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import SectionWrapper from '../hoc/SectionWrapper'
+
+const ServiceCard = ({ title, index, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center' >{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText} >Introduction</p>
+        <h2 className={styles.sectionHeadText} >Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        With 2.5 years of experience as a Full Stack Software Developer and React Native Developer, I bring
+        expertise in React.js, React Native, Redux, Next.js, Node.js, Express.js, AWS, MySQL, MongoDB,
+        JavaScript, Html5, CSS, Bootstrap and Mui to the table. I am on the lookout for an exciting and
+        challenging role with a renowned IT organization, where I can not only contribute to the company's
+        growth but also continue my journey of exploration and learning. My proficiency extends to Git, GitHub
+        and Docker. Let&apos;s embark on this exciting tech adventure together!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10' >
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Suspense, useEffect, useState } from 'react'
+import CanvasLoader from '../Loader'
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+  return (
+    <mesh>
+      <hemisphereLight intensity={4} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.65 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  )
+}
+
+// const Computers = () => {
+//   const computer = useGLTF('/desktop_pc/scene.gltf')
+//   if (!computer) return null; // Prevent rendering if model is not loaded
+//   return (
+//     <mesh>
+//       <hemisphereLight intensity={0.15} groundColor="black" />
+//       <pointLight intensity={2} />
+//       <spotLight
+//         position={[-20, 50, 10]}
+//         angle={0.12}
+//         penumbra={1}
+//         intensity={1}
+//         castShadow
+//         shadow-mapSize={1024}
+//       />
+//       <primitive
+//         object={computer.scene}
+//         scale={0.7}
+//         position={[0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   )
+// }
+
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    //Add a Listner for changes to the screen
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+    //set initial value of isMobile
+    setIsMobile(mediaQuery.matches);
+    //define a callback func to handle changes to the media querry
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    // Add the callback function as a listner for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+    //Remove the listner when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
+  return (
+    <Canvas
+      frameLoop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />} >
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputerCanvas
